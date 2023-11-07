e03757ce2cca04c6372800b4f49e8ab88992d52f
rijfas <rijfas01@gmail.com>
2023-11-07 22:21:54 +0530

    chore: setup urls

--- cgpu_connect/urls.py
+++ cgpu_connect/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('student/', include('student.urls', namespace='student')),
+    path('cgpu_admin/', include('cgpu_admin.urls', namespace='cgpu_admin')),
 ]
