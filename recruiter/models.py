bd5ee9f60925eb900ff5b7383647763ef1881c18
rijfas <rijfas01@gmail.com>
2023-11-13 13:39:33 +0530

    recruiter models completed

--- recruiter/models.py
+++ recruiter/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-#Imports from foreign apps
-from student.models import StudentProfile
+from student.models import Student, Department
 
 JOB_TYPE_CHOICES = (
     ('Internship', 'Internship'),
@@ -10,34 +9,50 @@ JOB_TYPE_CHOICES = (
     ('Part-Time', 'Part-Time'),
 )
 
-SHORTLIST_PROCESS_CHOICES = (
-    ('TEST', 'Test'),
-    ('TECH INT', 'Technical Interview'),
-    ('HR INT', 'HR Interview'),
-    ('PLCD', 'Placed')
+APPLICATION_STATUS_CHOICES = (
+    ('APL', 'Applied'),
+    ('VRF', 'Verified'),
+    ('REJ', 'Rejected'),
+    ('HOL', 'On Hold'),
+    ('TSP', 'Test Passed'),
+    ('TSF', 'Test Failed'),
+    ('SHR', 'Shortlisted For Interview'),
+    ('NSH', 'Not Shortlisted'),
+    ('CLR', 'Cleared the process'),
 )
 
-class RecruiterData(models.Model):
+SLAB_CHOICES = (
+    ('1', 'First'),
+    ('2', 'Second'),
+    ('3', 'Third'),
+    ('D', 'Dream'),
+)
+
+class Recruiter(models.Model):
     account = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
     mobile_no = models.CharField(max_length=15, blank=True, null=True)
     email_id = models.CharField(max_length=30, blank=False, null=False)
-    url = models.CharField(max_length=50)
+    website = models.CharField(max_length=50)
 
 class Job(models.Model):
-    recruiter = models.ForeignKey(RecruiterData, on_delete=models.CASCADE)
-    title = models.CharField(max_length=20)
+    recruiter = models.ForeignKey(Recruiter, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20)
     description = models.TextField()
-    #The following fields may or may not be required as the Job Description can cover them
-    posted_date = models.DateTimeField()
-    deadline = models.DateTimeField()
     job_type = models.CharField(max_length=10, choices=JOB_TYPE_CHOICES)
-    backlog_constraint = models.BooleanField(default=False)
-    active_backlog_constraint = models.BooleanField(default=False)
+    slab = models.CharField(max_length=10, choices=SLAB_CHOICES)
+    ctc = models.DecimalField(max_digits=5, decimal_places=2)
+    can_placed_students_apply = models.BooleanField()
+    posted_date = models.DateTimeField(auto_now_add=True)
+    application_deadline = models.DateTimeField()
+    maximum_backlogs = models.IntegerField(default=0)
+    maximum_active_backlogs = models.IntegerField(default=0)
     min_gpa = models.FloatField()
-    #eligible_courses
+    eligible_departments = models.ManyToManyField(Department)
 
-class Shortlist(models.Model):
+class Application(models.Model):
     job = models.ForeignKey(Job, on_delete=models.CASCADE)
-    process = models.CharField(max_length=30, choices=SHORTLIST_PROCESS_CHOICES)
-    students = models.ForeignKey(StudentProfile, on_delete=models.CASCADE, related_name='students_shortlist')
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    applied_on = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=3, choices=APPLICATION_STATUS_CHOICES, default='APL')
+    remarks = models.CharField(max_length=150, null=True, blank=True)
