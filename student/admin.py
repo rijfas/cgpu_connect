80a8dd16a432399c1b8a2e5331d656831fdc0033
rijfas <rijfas01@gmail.com>
2023-11-27 19:02:13 +0530

    registered all models in admin

--- student/admin.py
+++ student/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Student, AcademicQualification, Course, Department, Result
 # Register your models here.
+admin.site.register(Student)
+admin.site.register(AcademicQualification)
+admin.site.register(Course)
+admin.site.register(Department)
+admin.site.register(Result)
