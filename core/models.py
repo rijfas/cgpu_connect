dedaf9fa184ffb06e7e60b63c4c076c25184daff
rijfas <rijfas01@gmail.com>
2023-11-30 19:05:43 +0530

    created message model

--- core/models.py
+++ core/models.py
@@ -11,3 +11,11 @@ ACCOUNT_TYPE_CHOICES = (
 class Account(AbstractUser):
     type = models.CharField(max_length=10, choices=ACCOUNT_TYPE_CHOICES, default='student')
 
+class Message(models.Model):
+    created_on = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    sender = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='sender_of_message')
+    recepient = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='reciever_of_message')
+
+    def __str__(self):
+        return f'{self.sender}: {self.content}'
