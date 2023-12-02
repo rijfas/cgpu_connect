071eb73c6518fc4e289f50c7a808f83e5a8f9a31
rijfas <rijfas01@gmail.com>
2023-12-02 07:20:39 +0530

    added unread messages flag to user

--- core/models.py
+++ core/models.py
@@ -9,15 +9,21 @@ ACCOUNT_TYPE_CHOICES = (
     ('admin', 'Admin'),
 )
 
-class Account(AbstractUser):
-    type = models.CharField(max_length=15, choices=ACCOUNT_TYPE_CHOICES, default='student')
+
+
 
 class Message(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
-    sender = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='sender_of_message')
-    recepient = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='reciever_of_message')
+    sender = models.ForeignKey('Account', on_delete=models.CASCADE, related_name='sender_of_message')
+    recepient = models.ForeignKey('Account', on_delete=models.CASCADE, related_name='reciever_of_message')
     read = models.BooleanField(default=False)
 
     def __str__(self):
         return f'{self.sender}: {self.content}'
+
+class Account(AbstractUser):
+    type = models.CharField(max_length=15, choices=ACCOUNT_TYPE_CHOICES, default='student')
+
+    def has_unread_messages(self):
+        return Message.objects.filter(recepient=self, read=False).exists()
